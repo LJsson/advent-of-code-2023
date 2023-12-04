1619685fcd2b7c764c46ea8e45d7be4cdaac9cb7
LJsson <luddis6904@gmail.com>
2023-12-04 16:53:01 +0100

    added handling for braindamage

--- lib/day_1/part_two.ex
+++ lib/day_1/part_two.ex
@@ -21,6 +21,8 @@ defmodule Day1.PartTwo do
   end
 
   def pair_in_ints(input_line) do
+    input_line = split_number_combinations(input_line)
+
     regex = ~r/[1-9]|one|two|three|four|five|six|seven|eight|nine/
     matches = Regex.scan(regex, input_line)
     |> List.flatten()
@@ -34,7 +36,29 @@ defmodule Day1.PartTwo do
       end
     end)
     String.to_integer(first <> last)
+    |> IO.inspect()
 
+  end
+
+  def split_number_combinations(input_line) do
+    replacements = [
+      {"oneight", "oneeight"},
+      {"nineight", "nineeight"},
+      {"threeight", "threeeight"},
+      {"fiveight", "fiveeight"},
+      {"sevenine", "sevennine"},
+      {"eightwo", "eighttwo"},
+      {"eighthree", "eightthree"},
+      {"twone", "twoone"}
+    ]
+
+    Enum.reduce(replacements, input_line, fn {pattern, replacement}, acc ->
+      if String.contains?(acc, pattern) do
+        String.replace(acc, pattern, replacement)
+      else
+        acc
+      end
+    end)
   end
 
   def format_input({:ok, input}) do
